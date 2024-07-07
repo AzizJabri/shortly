53da1d9c3c307b58804e6f8b8ddbe286b6320046
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-07-07 02:11:12 +0100

    :bug: Fixed prod logger

--- server/server.js
+++ server/server.js
@@ -26,12 +26,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === "production") {
-    app.use(audit.createLogger({
-        excludeURLs: ["/api/v1"],
-        excludeHeaders: ["authorization"],
-        customFields: {
-            "user": "req.user",
-            "body": "req.body"
+    app.use(audit({
+        logger: logger, // Existing bunyan logger
+        excludeURLs: ['health', 'metrics'], // Exclude paths which enclude 'health' & 'metrics'
+        request: {
+            maskBody: ['password'], // Mask 'password' field in incoming requests
+            excludeHeaders: ['authorization'], // Exclude 'authorization' header from requests
+            excludeBody: ['creditCard'], // Exclude 'creditCard' field from requests body
+            maskHeaders: ['header1'], // Mask 'header1' header in incoming requests
+            maxBodyLength: 50 // limit length to 50 chars + '...'
+        },
+        response: {
+            maskBody: ['session_token'], // Mask 'session_token' field in response body
+            excludeHeaders: ['*'], // Exclude all headers from responses,
+            excludeBody: ['*'], // Exclude all body from responses
+            maskHeaders: ['header1'], // Mask 'header1' header in incoming requests
+            maxBodyLength: 50 // limit length to 50 chars + '...'
+        },
+        shouldSkipAuditFunc: function(req, res){
+            // Custom logic here.. i.e: return res.statusCode === 200
+            return false;
         }
     }));
 }
