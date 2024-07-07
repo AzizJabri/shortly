211a94e8ba9744085068490bb3641c8567dace8c
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-07-07 02:20:29 +0100

    :bug: Fixed loggin problems on production and migrated to morgan logger

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const audit = require("express-requests-logger");
+const logger = require('morgan');
 //ROUTES IMPORTS
 const auth = require("./src/routes/auth");
 
@@ -26,28 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 if(process.env.NODE_ENV === "production") {
-    app.use(audit({
-        logger: logger, // Existing bunyan logger
-        excludeURLs: ['health', 'metrics'], // Exclude paths which enclude 'health' & 'metrics'
-        request: {
-            maskBody: ['password'], // Mask 'password' field in incoming requests
-            excludeHeaders: ['authorization'], // Exclude 'authorization' header from requests
-            excludeBody: ['creditCard'], // Exclude 'creditCard' field from requests body
-            maskHeaders: ['header1'], // Mask 'header1' header in incoming requests
-            maxBodyLength: 50 // limit length to 50 chars + '...'
-        },
-        response: {
-            maskBody: ['session_token'], // Mask 'session_token' field in response body
-            excludeHeaders: ['*'], // Exclude all headers from responses,
-            excludeBody: ['*'], // Exclude all body from responses
-            maskHeaders: ['header1'], // Mask 'header1' header in incoming requests
-            maxBodyLength: 50 // limit length to 50 chars + '...'
-        },
-        shouldSkipAuditFunc: function(req, res){
-            // Custom logic here.. i.e: return res.statusCode === 200
-            return false;
-        }
-    }));
+    app.use(logger("combined"));
+}else{
+    app.use(logger("dev"));
 }
 
 
