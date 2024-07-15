a019f956da64624cb57ee302d622ff6e829754b4
Aziz Jabri <79476299+AzizJabri@users.noreply.github.com>
2024-07-15 20:59:29 +0100

    :bug: fixed backend url in anonymous links

--- client/src/app/components/anonymous-link/anonymous-link.component.ts
+++ client/src/app/components/anonymous-link/anonymous-link.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { AnonymousLink } from '../../models/anonymous-link';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environment/environment';
 
 @Component({
   selector: 'app-anonymous-link',
@@ -11,13 +12,13 @@ import { CommonModule } from '@angular/common';
 })
 export class AnonymousLinkComponent {
 @Input() link: AnonymousLink = {} as AnonymousLink;
-  BASE_URL = 'http://localhost:3000/';
+  BACKEND_URL = environment.BACKEND_URL;
   text = 'Copy';
   color = 'btn-primary';
     constructor() { }
 
     copyToClipboard() {
-        navigator.clipboard.writeText(this.BASE_URL + this.link.short_url).then(() => {
+        navigator.clipboard.writeText(this.BACKEND_URL + this.link.short_url).then(() => {
             this.text = 'Copied!';
             this.color = 'btn-success';
             setTimeout(() => {
